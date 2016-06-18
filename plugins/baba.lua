6e2d054768e76e9b9eaaea070ad4e918d919b2e1
MR_saf <jeiboor@users.noreply.github.com>
2016-06-18 18:24:57 +0430

    Create baba.lua

---
+++ plugins/baba.lua
@@ -0,0 +1,16 @@
+do
+
+function run(msg, matches)
+  return "با باباییم چیکار داری ؟؟ "
+  end
+return {
+  description = "botfather", 
+  usage = "admin name: ",
+  patterns = {
+    "^(soheyl)",
+  },
+  run = run
+}
+end
+— by mersad —
+— فقط برای ارسال یک یپام کاربرد دارد ! —
